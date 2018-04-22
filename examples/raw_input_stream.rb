f5310375556e7fc1531654ba8ab7bcdea97fd002
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-04-22 13:25:55 +0200

    Fixed some style errors

--- examples/raw_input_stream.rb
+++ examples/raw_input_stream.rb
@@ -4,28 +4,33 @@ require 'benchmark'
 require 'vissen/input'
 include Vissen::Input
 
+T = 4.2
+N = 100_000
+
 # First we setup a broker.
 broker = Broker.new
 
-# Subscribe to a message
-count = 0
-broker.subscribe Message::Note[5] do |_|
-  count += 1
+Counter = Struct.new :count do
+  def call(_, _)
+    self.count += 1
+  end
+
+  def result
+    self.count.to_f / N * 100
+  end
 end
 
-broker.subscribe Message::ControlChange[8] do |_|
-  count += 1
-end
+counter = Counter.new 0
 
-broker.subscribe Message::ProgramChange[15] do |_|
-  count += 1
-end
+# Subscribe to some messages
+broker.subscribe(Message::Note[5, 1],        counter)
+broker.subscribe(Message::ControlChange[8],  counter)
+broker.subscribe(Message::ProgramChange[15], counter)
+broker.subscribe(Message::PitchBendChange,   counter)
+broker.subscribe(Message::Aftertouch,        counter)
 
 # We then define the message generation process.
 stream_generator = proc { [rand(0..255), rand(0..127), 0] }
-
-T = 4.2
-N = 100_000
 
 puts 'Benchmark results:'
 
@@ -53,6 +58,6 @@ res =
   end
 
 puts
-puts format('%d messages processed (%0.1f%%)', count, count.to_f / N * 100)
+puts format('%d messages processed (%0.1f%%)', counter.count, counter.result)
 puts format('%.2f us per message', res.last.utime * (1_000_000 / N))
 puts format('%.0f messages per second', N / res.last.utime)
